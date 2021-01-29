9e4a5c8ea8a36bb4401932cc702f0aeab2041c7a
MSI\ShayK <shay.krainer@gmail.com>
2021-01-29 20:28:37 +0200

    following NPC actually works

--- I'm losing it!!/Assets/Scripts/NPCController.cs
+++ I'm losing it!!/Assets/Scripts/NPCController.cs
@@ -13,7 +13,7 @@ public class NPCController : MonoBehaviour
 {
     private CharacterMover characterMover;
 
-    private NPCState state = NPCState.Idling;
+    public NPCState state = NPCState.Idling;
     private CharacterController characterController;
 
     public PointsOfInterest[] pointsOfInterest;
@@ -41,6 +41,13 @@ public class NPCController : MonoBehaviour
             if (allowDebug)
             {
                 BecomePlayable();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (allowDebug)
+            {
+                StartFollowingPlayerToTarget(target);
             }
         }
     }
@@ -112,7 +119,7 @@ public class NPCController : MonoBehaviour
         lastStretch = transform.DOMove(pointsOfInterest[currentPointIdx].transform.position, 0.03f).OnComplete(OnDone);
     }
 
-    public void StartFollowingPlayer(PointsOfInterest target)
+    public void StartFollowingPlayerToTarget(PointsOfInterest target)
     {
         this.target = target;
         SetState(NPCState.Follow);
@@ -121,21 +128,31 @@ public class NPCController : MonoBehaviour
     public void FollowPlayer()
     {
         followee = GameObject.FindObjectOfType<CinemachineVirtualCamera>().Follow;
+        StartCoroutine(FollowToTarget());
     }
     
-    IEnumerator Follow()
+    IEnumerator FollowToTarget()
     {
         
         while (state == NPCState.Follow)
         {
-            if (Vector3.Distance(_transform.position, target.transform.position) > 3)
+            if (Vector3.Distance(_transform.position, target.transform.position) > 1)
             {
-                direction = followee.position - _transform.position;
-                characterMover.Move(direction.normalized);
-                yield return null;
+                if (Vector3.Distance(_transform.position, followee.position) > 1)
+                {
+                    direction = followee.position - _transform.position;
+                    characterMover.Move(direction.normalized);
+                    yield return null;
+                }
+                else
+                {
+                    characterMover.Move(Vector3.zero);
+                    yield return null;
+                }
             }
             else
             {
+                characterMover.Move(Vector3.zero);
                 if (Vector3.Distance(_transform.position, target.transform.position) > 0.5f)
                 {
                     transform.DOMove(target.transform.position, 0.5f);
