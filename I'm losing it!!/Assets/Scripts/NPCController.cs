31b1e8cca146ea50ad24a84fdfe0344e7a32f12a
MSI\ShayK <shay.krainer@gmail.com>
2021-01-29 19:57:34 +0200

    following mechanic added to NPC

--- I'm losing it!!/Assets/Scripts/NPCController.cs
+++ I'm losing it!!/Assets/Scripts/NPCController.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 
 public enum NPCState
 {
-    Moving, Working, Annoying, Idling
+    Moving, Working, Annoying, Idling, Follow
 }
 public class NPCController : MonoBehaviour
 {
@@ -18,6 +18,8 @@ public class NPCController : MonoBehaviour
 
     public PointsOfInterest[] pointsOfInterest;
     public int currentPointIdx = -1;
+    public PointsOfInterest target;
+    public Transform followee;
 
     private Transform _transform;
     private Vector2 direction;
@@ -66,6 +68,9 @@ public class NPCController : MonoBehaviour
                     break;
                 case NPCState.Annoying:
                     break;
+                case NPCState.Follow:
+                    FollowPlayer();
+                    break;
         }
     }
 
@@ -105,5 +110,48 @@ public class NPCController : MonoBehaviour
         }
 
         lastStretch = transform.DOMove(pointsOfInterest[currentPointIdx].transform.position, 0.03f).OnComplete(OnDone);
+    }
+
+    public void StartFollowingPlayer(PointsOfInterest target)
+    {
+        this.target = target;
+        SetState(NPCState.Follow);
+    }
+    
+    public void FollowPlayer()
+    {
+        followee = GameObject.FindObjectOfType<CinemachineVirtualCamera>().Follow;
+    }
+    
+    IEnumerator Follow()
+    {
+        
+        while (state == NPCState.Follow)
+        {
+            if (Vector3.Distance(_transform.position, target.transform.position) > 3)
+            {
+                direction = followee.position - _transform.position;
+                characterMover.Move(direction.normalized);
+                yield return null;
+            }
+            else
+            {
+                if (Vector3.Distance(_transform.position, target.transform.position) > 0.5f)
+                {
+                    transform.DOMove(target.transform.position, 0.5f);
+                    yield return new WaitForSeconds(0.5f);
+                }
+                else
+                {
+                    yield return null;
+                }
+
+            }
+        }
+    }
+
+    public void StopFollowing()
+    {
+        SetState(NPCState.Idling);
     }
 }
